79acd45c9fbf1186225d8d600f550c658b5d9a12
Aidan <aidan@thewizardscat.co.nz>
2025-01-04 19:38:28 +1300

    [ToolMouse] start collision processing

--- src/systems/ToolMouse.cpp
+++ src/systems/ToolMouse.cpp
@@ -1,8 +1,19 @@
-#include <SFML/Window/Event.hpp>
 #include <iostream>
-#include <systems/ToolClick.h>
+
+#include <systems/ToolMouse.h>
+#include <SFML/Window/Event.hpp>
+#include <engine/Components.hpp>
 
 void ToolMouse::handle_click(const sf::Event::MouseButtonPressed* evt) {
+
+	for (auto e=registered_entities.begin(); e != registered_entities.end(); e++) {
+		Entity ent = (Entity) *e;
+
+		Collider col = component_manager->get_component_data<Collider>(ent);
+
+	}
+
 	std::cout << "Tool Clicking" << std::endl;
+
 }
 
