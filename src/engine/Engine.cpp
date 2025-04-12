9c90bf32879d21a301cf342f2b9c2af4ab5cf432
Aidan <aidan@thewizardscat.co.nz>
2025-04-12 18:35:04 +1200

    [SDL] get open gl context and window is black!!!

--- src/engine/Engine.cpp
+++ src/engine/Engine.cpp
@@ -3,58 +3,62 @@
 
 #include <ProjectConfig.h>
 #include <SDL3/SDL.h>
+#include <SDL3/SDL_opengl.h>
+#include <SDL3/SDL_video.h>
 
 /** Basically just the state machine to switch between pause and running */
 
 Engine::Engine() {
 
-	SDL_Init(SDL_INIT_VIDEO);
+  SDL_Init(SDL_INIT_VIDEO);
 
-
-	state_manager.set_state(std::make_shared<MainState>());
-	
-	
+  state_manager.set_state(std::make_shared<MainState>());
 }
 
 void Engine::run() {
-	
-	window = SDL_CreateWindow(
-		"This Window",
-		800,
-		600,
-SDL_WINDOW_OPENGL
-	);
 
-	std::cout << window << std::endl;
+  window = SDL_CreateWindow("This Window", 800, 600, SDL_WINDOW_OPENGL);
+  SDL_Renderer *renderer;
 
-	bool window_is_open = true;
-	while (window_is_open) {
-		//window->height
+  // SDL_CreateWindowAndRenderer("This Winto", 800, 600, SDL_WINDOW_OPENGL,
+  //                             &window, &renderer);
 
-		//std::shared_ptr<GameState> current_state = state_manager.get_current_state();
+  SDL_GLContext ctx = SDL_GL_CreateContext(window);
+  std::cout << window << std::endl;
 
-		/** Event based */
+  // ctx.glClear(0, 0, 0, 1);
+  // glClearColor(0, 0, 0, 1); // TODO: why this doesn't work???
+  // glClear(GL_COLOR_BUFFER_BIT);
+  SDL_GL_SwapWindow(window);
+  bool window_is_open = true;
+  while (window_is_open) {
+    // window->height
 
+    // std::shared_ptr<GameState> current_state =
+    // state_manager.get_current_state();
 
-		/* Update */
-		//float dt = clock.restart().asSeconds();
-		// float dt = 0.016;
-		//
-		// current_state->update(dt);
+    /** Event based */
+    SDL_Event event;
 
-		/* Draw */
-		SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        window_is_open = false;
+      }
+    }
 
-		while (SDL_PollEvent(&event)) {
-		    if (event.type == SDL_EVENT_QUIT) {
-			window_is_open = false;
-		    }
-		}
+    /* Update */
+    // float dt = clock.restart().asSeconds();
+    //  float dt = 0.016;
+    //
+    //  current_state->update(dt);
 
-			// Do game logic, present a frame, etc.
+    /* Draw */
 
-	}
+    // SDL_RenderClear(renderer);
+    // SDL_RenderPresent(renderer);
+  }
 
-	SDL_DestroyWindow(window);
-	SDL_Quit();
+  SDL_DestroyWindow(window);
+  SDL_GL_DestroyContext(ctx);
+  SDL_Quit();
 }
--- src/main.cpp
+++ src/main.cpp
@@ -2,50 +2,15 @@
 #include <ctime>
 #include <iostream>
 
-#include <engine/Engine.h>
 #include <ProjectConfig.h>
-
+#include <engine/Engine.h>
 
 int main() {
 
-	// std::cout << "Graphics Path: " << GRAPHICS_PATH << std::endl;
-	//
-	// srand(time(NULL));
-	//
-	// Engine game;
-	// game.run();
-	
-	SDL_Window *window = SDL_CreateWindow("Winto", 800, 600, SDL_WINDOW_OPENGL);
+  std::cout << "Graphics Path: " << GRAPHICS_PATH << std::endl;
 
-	bool window_is_open = true;
+  srand(time(NULL));
 
-	while (window_is_open) {
-		//window->height
-
-		//std::shared_ptr<GameState> current_state = state_manager.get_current_state();
-
-		/** Event based */
-
-
-		/* Update */
-		//float dt = clock.restart().asSeconds();
-		// float dt = 0.016;
-		//
-		// current_state->update(dt);
-
-		/* Draw */
-		SDL_Event event;
-
-		while (SDL_PollEvent(&event)) {
-		    if (event.type == SDL_EVENT_QUIT) {
-			window_is_open = false;
-		    }
-		}
-
-			// Do game logic, present a frame, etc.
-
-	}
-	
-
-
+  Engine game;
+  game.run();
 }
