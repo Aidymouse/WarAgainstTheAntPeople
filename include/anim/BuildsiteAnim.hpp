42da479146300088ba4cf5aefed1301152f2537c
Aidan <aidan@thewizardscat.co.nz>
2025-06-26 00:38:55 +1200

    [Buildsite] add buildsite anims

---
+++ include/anim/BuildsiteAnim.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <anim/Anim.hpp>
+
+struct {
+  AnimFrame BUILD1 = {{0, 0, 32, 64}, -1, &BUILD2};
+  AnimFrame BUILD2 = {{1 * 32, 0, 32, 64}, -1, &BUILD3};
+  AnimFrame BUILD3 = {{2 * 32, 0, 32, 64}, -1, &BUILD4};
+  AnimFrame BUILD4 = {{3 * 32, 0, 32, 64}, -1, nullptr};
+} BuildsiteAnim;
