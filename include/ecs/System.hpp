6dbd7b56d00d514138702345d17d7c334f86a921
Aidan <aidan@thewizardscat.co.nz>
2025-07-03 19:17:25 +1200

    [ECS] Add debug cout to system.hpp

--- include/ecs/System.hpp
+++ include/ecs/System.hpp
@@ -12,9 +12,29 @@ public:
 
   void register_entity(Entity id) { registered_entities.insert(id); }
 
-  void remove_entity(Entity id) { registered_entities.erase(id); }
+	void remove_entity(Entity id) { 
+		registered_entities.erase(id);
+ 	}
 
   void set_component_manager(std::shared_ptr<ComponentManager> manager) {
     component_manager = manager;
   }
+
+	void debug_cout_entities() {
+
+		std::cout << registered_entities.empty();
+/*
+		int num_ents = registered_entities.size();
+		std::cout << "System has " << num_ents << " entities ";
+		if (num_ents > 0) {
+			for (auto e = registered_entities.begin(); e != registered_entities.end(); e++) {
+				Entity ent = (Entity) *e;
+				std::cout << "[" << ent << "] ";
+			}
+		}
+*/
+		std::cout << std::endl;
+
+
+	}
 };
