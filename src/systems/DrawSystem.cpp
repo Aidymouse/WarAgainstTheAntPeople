732d310d70974792e0f9c11ca31d004f19065107
Aidan <aidan@thewizardscat.co.nz>
2025-07-16 22:17:47 +1200

    [DrawSystem] outsource updating anim timer and drawing component to pass by reference functions
    I've been neglecting this core part of c++ for a year :)

--- src/systems/DrawSystem.cpp
+++ src/systems/DrawSystem.cpp
@@ -20,58 +20,72 @@ static int compare_visibles_by_position(SortedVisible v1, SortedVisible v2,
   return 0;
 }
 
+void update_anim_timer(float dt, float& anim_timer, AnimFrame& frame) {
+	if (anim_timer != -1 && frame.duration != -1) {
+
+		anim_timer += dt;
+
+		if (anim_timer > frame.duration) {
+
+			anim_timer -= frame.duration;
+        	// TODO: more robustness with skipping frames. There could potentially be multiple frames skipped a frame, current set up will only ever get one
+
+        	if (frame.next_frame != nullptr) {
+          		//AnimFrame a = *frame.next_frame;
+          		frame = *(frame.next_frame);
+        	} else {
+          		std::cout << "Animation timer ran out but no next frame! Locking." << std::endl;
+          		anim_timer = -1;
+        	}
+		}
+	}
+}
+
 // The draw system is responsible for two types of drawable entities - sorted and unsorted
 // It also manages putting things in sorted vs unsorted
 // TODO one day it will handle decorations too
 void DrawSystem::update(float dt, ECS *ecs) {
 
-  // Update animation timers TODO
-  for (auto e = registered_entities.begin(); e != registered_entities.end();
-       e++) {
-    Entity ent = (Entity)*e;
-    Visible *vis = component_manager->get_component_data<Visible>(ent);
-    if (vis->anim_timer != -1 && vis->frame.duration != -1) {
+	/** Update animation timers */
+	// Update Decorations
+	std::shared_ptr<ComponentArray<Decoration>> decorations = component_manager->get_component_array<Decoration>();
+  	for (int i = 0; i < decorations->get_num_components(); i++) {
 
-      vis->anim_timer += dt;
+		Entity ent = decorations->get_entity_from_idx(i);
+		Decoration *d_vis = decorations->get_editable_data_from_idx(i);
+		
+	}
 
-      if (vis->anim_timer > vis->frame.duration) {
+	std::shared_ptr<ComponentArray<Visible>> comp_visible = component_manager->get_component_array<Visible>();
+  	for (int i = 0; i < comp_visible->get_num_components(); i++) {
 
-        vis->anim_timer -= vis->frame.duration;
-        // TODO: more robustness with skipping frames
-
-        if (vis->frame.next_frame != nullptr) {
-          AnimFrame a = *vis->frame.next_frame;
-          vis->frame = *(vis->frame.next_frame);
-        } else {
-          std::cout << "Animation timer ran out but no next frame! Locking."
-                    << std::endl;
-          vis->anim_timer = -1;
-        }
-      }
-    }
-  }
+		Entity ent = comp_visible->get_entity_from_idx(i);
+		Visible *vis = comp_visible->get_editable_data_from_idx(i);
+		
+		update_anim_timer(dt, vis->anim_timer, vis->frame);
+	}
 
 	// Sort the sorted drawables
-  std::shared_ptr<ComponentArray<SortedVisible>> visibles = component_manager->get_component_array<SortedVisible>();
-  visibles->insertion_sort(&compare_visibles_by_position, ecs);
+	std::shared_ptr<ComponentArray<SortedVisible>> visibles = component_manager->get_component_array<SortedVisible>();
+	visibles->insertion_sort(&compare_visibles_by_position, ecs);
 }
 
 
-void render_component(SDL_Renderer *renderer, ECS *ecs, Visible vis, Position *pos) {
+void render_component(SDL_Renderer *renderer, ECS *ecs, SDL_Texture* texture, AnimFrame& frame, Position& pos, xy& offset) {
 	/** Draw Shadow if in air */
-	if (pos->z > 0) {
+	if (pos.z > 0) {
     	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 128);
-		int shadow_radius = vis.frame.rect.w/1.5 - pos->z*5;
+		int shadow_radius = frame.rect.w/1.5 - pos.z*5;
 		if (shadow_radius < 10) { shadow_radius = 10; }
-		DrawFns::RenderFilledCircle(renderer, pos->x, pos->y, shadow_radius);
+		DrawFns::RenderFilledCircle(renderer, pos.x, pos.y, shadow_radius);
 	}
 
-    SDL_FRect source_rect = vis.frame.rect;
-    SDL_FRect target_rect = {std::floor(pos->x + vis.offset.x + vis.frame.offset_x),
-                             std::floor(pos->y + vis.offset.y + vis.frame.offset_y - pos->z),
-                             vis.frame.rect.w, vis.frame.rect.h};
+    SDL_FRect source_rect = frame.rect;
+    SDL_FRect target_rect = {std::floor(pos.x + offset.x + frame.offset_x),
+                             std::floor(pos.y + offset.y + frame.offset_y - pos.z),
+                             frame.rect.w, frame.rect.h};
     // SDL_FRect target_rect = {pos->x, pos->y, 16, 16};
-    SDL_RenderTexture(renderer, vis.texture, &source_rect, &target_rect);
+    SDL_RenderTexture(renderer, texture, &source_rect, &target_rect);
 }
 
 
@@ -80,38 +94,49 @@ void render_component(SDL_Renderer *renderer, ECS *ecs, Visible vis, Position *p
 // This simplistic draw system just draws everything visible. It sorts things marked sortable
 void DrawSystem::draw(SDL_Renderer *renderer, ECS *ecs) {
 
-  std::shared_ptr<ComponentArray<Visible>> visibles = component_manager->get_component_array<Visible>();
-  //std::shared_ptr<ComponentArray<Position>> positions = component_manager->get_component_array<Position>();
 
-	//std::shared_ptr<ComponentArray<Visible>> sorted_visibles_c = std::static_pointer_cast<ComponentArray<Visible>>(sorted_visibles);
+	// Decorations
+/*
+	std::shared_ptr<ComponentArray<Decoration>> decorations = component_manager->get_component_array<Decoration>();
+  	for (int i = 0; i < decorations->get_num_components(); i++) {
+
+		Entity ent = decorations->get_entity_from_idx(i);
+		Decoration d_vis = decorations->get_data_from_idx(i);
+
+		Visible vis;
+		vis.texture = d_vis.texture;
+		vis.frame = d_vis.frame;
+		vis.offset.x = d_vis.offset.x;
+		vis.offset.y = d_vis.offset.y;
+
+		Position *pos = ecs->get_component_for_entity<Position>(ent);
+		render_component(renderer, ecs, vis, pos);
+	}
+*/
+
+
+  std::shared_ptr<ComponentArray<Visible>> visibles = component_manager->get_component_array<Visible>();
 
   for (int i = 0; i < visibles->get_num_components(); i++) {
 
     Entity ent = visibles->get_entity_from_idx(i);
     Visible vis = visibles->get_data_from_idx(i);
-    Position *pos = ecs->get_component_for_entity<Position>(ent);
+    Position pos = *ecs->get_component_for_entity<Position>(ent);
 
-	render_component(renderer, ecs, vis, pos);
+	render_component(renderer, ecs, vis.texture, vis.frame, pos, vis.offset);
   }
 
-/** Idk if this is horribly slow or what but we can convert a visible into a sorted visible pretty easily */
-// My biggest worry would be the stack frames calling a func for every entity but surely the component is smart enough to inline here...
-	std::shared_ptr<ComponentArray<SortedVisible>> sorted_visibles = component_manager->get_component_array<SortedVisible>();
+  std::shared_ptr<ComponentArray<SortedVisible>> sorted_visibles = component_manager->get_component_array<SortedVisible>();
+
   for (int i = 0; i < sorted_visibles->get_num_components(); i++) {
 
     Entity ent = sorted_visibles->get_entity_from_idx(i);
     SortedVisible s_vis = sorted_visibles->get_data_from_idx(i);
+    Position pos = *ecs->get_component_for_entity<Position>(ent);
 
-	Visible vis;
-	vis.texture = s_vis.texture;
-	vis.frame = s_vis.frame;
-	vis.offset.x = s_vis.offset.x;
-	vis.offset.y = s_vis.offset.y;
-
-    Position *pos = ecs->get_component_for_entity<Position>(ent);
-
-	render_component(renderer, ecs, vis, pos);
+	render_component(renderer, ecs, s_vis.texture, s_vis.frame, pos, s_vis.offset);
   }
 
 }
+
 
