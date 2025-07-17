f0db18e7513111102f72d90e5b5384feefce8217
Aidan <aidan@thewizardscat.co.nz>
2025-07-17 21:32:18 +1200

    [BuildSystem] format

--- src/systems/BuildSystem.cpp
+++ src/systems/BuildSystem.cpp
@@ -21,38 +21,37 @@ void BuildSystem_check_resources(float dt, ECS *ecs, CollisionGrid *grid) {
 
 	TextureStore &b_texture_store = TextureStore::getInstance();
 
-  std::shared_ptr<ComponentArray<Resource>> comp_resource =
-      ecs->get_component_array<Resource>();
+	std::shared_ptr<ComponentArray<Resource>> comp_resource = ecs->get_component_array<Resource>();
 
-  for (int r = 0; r < comp_resource->get_num_components(); r++) {
-    Entity resource_id = comp_resource->get_entity_from_idx(r);
+	for (int r = 0; r < comp_resource->get_num_components(); r++) {
+		Entity resource_id = comp_resource->get_entity_from_idx(r);
 
-    //std::cout << "Checking build system for [" << resource_id << "]" << std::endl;
-    // If two resources are touching and at least one of them is being carried
-    // then they should transform into a buildsite
-    Collider *resource_c = ecs->get_component_for_entity<Collider>(resource_id);
+		//std::cout << "Checking build system for [" << resource_id << "]" << std::endl;
+		// If two resources are touching and at least one of them is being carried
+		// then they should transform into a buildsite
+		Collider *resource_c = ecs->get_component_for_entity<Collider>(resource_id);
 
-    bool is_carried = false;
-    if (ecs->entity_has_component<Carryable>(resource_id)) {
-      Carryable *resource_e =
-          ecs->get_component_for_entity<Carryable>(resource_id);
+		bool is_carried = false;
+		if (ecs->entity_has_component<Carryable>(resource_id)) {
+			Carryable *resource_e =
+					ecs->get_component_for_entity<Carryable>(resource_id);
 
-      if (resource_e->carriers_count > 0) {
-        is_carried = true;
-      }
-    }
+			if (resource_e->carriers_count > 0) {
+				is_carried = true;
+			}
+		}
 
-    std::set<Entity> collided_resources = grid->get_collisions(*resource_c, ecs);
+		std::set<Entity> collided_resources = grid->get_collisions(*resource_c, ecs);
 
-    for (auto resource_e = collided_resources.begin(); resource_e != collided_resources.end(); resource_e++) {
-      Entity collided_resource_ent = (Entity)*resource_e;
-      if (!ecs->entity_has_component<Resource>(collided_resource_ent)) { continue; }
+		for (auto resource_e = collided_resources.begin(); resource_e != collided_resources.end(); resource_e++) {
+			Entity collided_resource_ent = (Entity)*resource_e;
+			if (!ecs->entity_has_component<Resource>(collided_resource_ent)) { continue; }
 
-      if (collided_resource_ent == resource_id) { continue; }
+			if (collided_resource_ent == resource_id) { continue; }
 
-      if ((ecs->entity_has_component<Carryable>(collided_resource_ent) &&
-           ecs->get_component_for_entity<Carryable>(collided_resource_ent) ->carriers_count > 0) ||
-          is_carried) {
+			if ((ecs->entity_has_component<Carryable>(collided_resource_ent) &&
+					 ecs->get_component_for_entity<Carryable>(collided_resource_ent) ->carriers_count > 0) ||
+					is_carried) {
 		// Remove whatever i collided with
 		// Whatever was carrying this will get sorted out in the carry system
 		// Need to update all the same stuff as the resource we collided with too though!!!
@@ -73,24 +72,24 @@ void BuildSystem_check_resources(float dt, ECS *ecs, CollisionGrid *grid) {
 			
 		}
 		grid->remove_entity(collided_resource_ent);
-        ecs->remove_entity(collided_resource_ent);
+				ecs->remove_entity(collided_resource_ent);
 
-        // Turn me into buildsite 
-        // Tower for now cos i only have one
-        ecs->remove_component_from_entity<Resource>(resource_id);
+				// Turn me into buildsite 
+				// Tower for now cos i only have one
+				ecs->remove_component_from_entity<Resource>(resource_id);
 
-        ecs->remove_component_from_entity<Carryable>(resource_id);
+				ecs->remove_component_from_entity<Carryable>(resource_id);
 		if (ecs->entity_has_component<Position>(resource_id)) {
 			Position *p = ecs->get_component_for_entity<Position>(resource_id);
 			p->z = 0;
 		}
 
-        ecs->remove_component_from_entity<ScanningFor>(resource_id);
-        ecs->remove_component_from_entity<ScanningFor>(resource_id);
-        ecs->remove_component_from_entity<Persuing>(resource_id);
-        ecs->remove_component_from_entity<Transform>(resource_id);
+				ecs->remove_component_from_entity<ScanningFor>(resource_id);
+				ecs->remove_component_from_entity<ScanningFor>(resource_id);
+				ecs->remove_component_from_entity<Persuing>(resource_id);
+				ecs->remove_component_from_entity<Transform>(resource_id);
 
-        Buildable b = {
+				Buildable b = {
 			0,
 			4,
 			{ bs_TowerAnim.BUILD1, bs_TowerAnim.BUILD2, bs_TowerAnim.BUILD3, bs_TowerAnim.BUILD4, },
@@ -112,20 +111,23 @@ void BuildSystem_check_resources(float dt, ECS *ecs, CollisionGrid *grid) {
 		sv.offset.x = v->offset.x;
 		sv.offset.y = v->offset.y;
 
-        	ecs->add_component_to_entity<SortedVisible>(resource_id, sv);
-        	ecs->remove_component_from_entity<Visible>(resource_id);
+		ecs->add_component_to_entity<SortedVisible>(resource_id, sv);
+		ecs->remove_component_from_entity<Visible>(resource_id);
 
-        	ecs->add_component_to_entity<Buildable>(resource_id, b);
+		ecs->add_component_to_entity<Buildable>(resource_id, b);
 	
 		Scannable *s = ecs->get_component_for_entity<Scannable>(resource_id);
 		s->scan_value = SCAN_VALUES::SV_BUILDSITE_WANT_SCRAP;
 
 		if (ecs->entity_has_component<hv_Brain>(resource_id)) {
-			// Resource is being carried most likely
+			// Resource is being carried most likely ... I can check that this is true...
 			hv_Brain *hv = ecs->get_component_for_entity<hv_Brain>(resource_id);
 
 			for (int e = 0; e<hv->num_entities; e++){
-				ecs->add_component_to_entity<ScanningFor>(hv->entities[e], {{SCAN_VALUES::SV_CARRIED_SCRAP, SCAN_VALUES::SV_SCRAP_METAL, -1, -1}, {GuyAttrs.scan_range, GuyAttrs.scan_range, 0, 0}});
+				ecs->add_component_to_entity<ScanningFor>(hv->entities[e], {
+					{SCAN_VALUES::SV_CARRIED_SCRAP, SCAN_VALUES::SV_SCRAP_METAL, -1, -1}, 
+					{GuyAttrs.scan_range, GuyAttrs.scan_range, 0, 0}
+				});
 				ecs->add_component_to_entity<HandsFree>(hv->entities[e], {});
 			}
 
@@ -133,9 +135,9 @@ void BuildSystem_check_resources(float dt, ECS *ecs, CollisionGrid *grid) {
 			
 		}
 
-      }
-    }
-  }
+			}
+		}
+	}
 }
 
 void BuildSystem_add_resource_to_buildsite(ECS *ecs, Entity buildsite_id, Entity resource_id) {
@@ -166,7 +168,10 @@ void BuildSystem_check_buildsites(float dt, ECS *ecs, CollisionGrid *grid) {
 						hv_Brain *hv = ecs->get_component_for_entity<hv_Brain>(collided_ent);
 
 						for (int e = 0; e<hv->num_entities; e++){
-							ecs->add_component_to_entity<ScanningFor>(hv->entities[e], {{SCAN_VALUES::SV_SCRAP_METAL, -1, -1, -1}, {GuyAttrs.scan_range, 0, 0, 0}});
+							ecs->add_component_to_entity<ScanningFor>(hv->entities[e], {
+								{SCAN_VALUES::SV_SCRAP_METAL, -1, -1, -1}, 
+								{GuyAttrs.scan_range, 0, 0, 0}
+							});
 							ecs->add_component_to_entity<HandsFree>(hv->entities[e], {});
 						}
 						dissolve_hivemind(ecs, collided_ent);
