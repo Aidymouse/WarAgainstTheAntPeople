5af899ee82b710e4b7e2ec91c2c72e655904c6ad
Aidan <aidymouse4891@gmail.com>
2025-07-18 23:33:04 +1200

    [Anim] add correct offsets

--- include/anim/BuildsiteAnim.hpp
+++ include/anim/BuildsiteAnim.hpp
@@ -3,8 +3,8 @@
 #include <anim/Anim.hpp>
 
 struct {
-  AnimFrame BUILD1 = {{0, 0, 32, 64}, -1, &BUILD2, 0, 0};
-  AnimFrame BUILD2 = {{1 * 32, 0, 32, 64}, -1, &BUILD3, 0, 0};
-  AnimFrame BUILD3 = {{2 * 32, 0, 32, 64}, -1, &BUILD4, 0, 0};
-  AnimFrame BUILD4 = {{3 * 32, 0, 32, 64}, -1, nullptr, 0, 0};
+  AnimFrame BUILD1 = {{0, 0, 32, 64}, -1, &BUILD2, -16, -64};
+  AnimFrame BUILD2 = {{1 * 32, 0, 32, 64}, -1, &BUILD3, -16, -64};
+  AnimFrame BUILD3 = {{2 * 32, 0, 32, 64}, -1, &BUILD4, -16, -64};
+  AnimFrame BUILD4 = {{3 * 32, 0, 32, 64}, -1, nullptr, -16, -64};
 } bs_TowerAnim;
