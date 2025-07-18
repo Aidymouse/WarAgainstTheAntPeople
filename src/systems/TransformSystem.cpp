f714fd8cf9a93bb2c42f7d2f7a96e46235348289
Aidan <aidymouse4891@gmail.com>
2025-07-18 18:38:45 +1200

    [TransformSystem] apply speed multiplier for carryable things based on number of carriers

--- src/systems/TransformSystem.cpp
+++ src/systems/TransformSystem.cpp
@@ -4,27 +4,33 @@
 
 void TransformSystem::update(float dt, CollisionGrid *grid, ECS *ecs) {
 
-  for (auto e = registered_entities.begin(); e != registered_entities.end();
-       e++) {
-    Entity ent = (Entity)*e;
-    Position *pos = component_manager->get_component_data<Position>(ent);
-    Transform *trans = component_manager->get_component_data<Transform>(ent);
+	for (auto e = registered_entities.begin(); e != registered_entities.end(); e++) {
+		Entity ent = (Entity)*e;
+		Position *pos = component_manager->get_component_data<Position>(ent);
+		Transform *trans = component_manager->get_component_data<Transform>(ent);
 
-    pos->x += trans->vel_x * dt;
-    pos->y += trans->vel_y * dt;
+	float speed_multiplier = 1;
 
-    //std::cout << "Trans [" << ent << "] " << trans->vel_x << ", " << trans->vel_y << std::endl;
+	if (ecs->entity_has_component<Carryable>(ent)) {
+		Carryable *c = ecs->get_component_for_entity<Carryable>(ent);
+		speed_multiplier = std::min((float)c->carrier_effort / (float)c->weight, 1.f);
+		//std::cout << "Speed Multiplier [" << ent << "]: " << speed_multiplier << std::endl;
+	}
 
-    if (ecs->entity_has_component<Collider>(ent)) {
-      Collider *c = component_manager->get_component_data<Collider>(ent);
-      // std::cout << "Collider for " << ent << ", " << c << std::endl;
+	pos->x += trans->vel_x * speed_multiplier * dt;
+	pos->y += trans->vel_y * speed_multiplier * dt;
 
-      Collisions::update_collider_position(c, pos->x, pos->y);
+	//std::cout << "Trans [" << ent << "] " << trans->vel_x << ", " << trans->vel_y << std::endl;
 
-      grid->update_entity(ent, *pos, *c);
-    }
-    // grid->update_entity(ent, *pos);
+	if (ecs->entity_has_component<Collider>(ent)) {
+		Collider *c = component_manager->get_component_data<Collider>(ent);
+		// std::cout << "Collider for " << ent << ", " << c << std::endl;
 
-    // If we were gonna do acceleration or anything we do it here
-  }
+		Collisions::update_collider_position(c, pos->x, pos->y);
+
+		grid->update_entity(ent, *pos, *c);
+	}
+
+	// If we were gonna do acceleration or anything we do it here
+	}
 };
