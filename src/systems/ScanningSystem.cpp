123b43ae4e24f22b33d5be70a6ed6bb3bd10d28b
Aidan <aidymouse4891@gmail.com>
2025-07-18 18:38:22 +1200

    [ScanningSystem] format + comments

--- src/systems/ScanningSystem.cpp
+++ src/systems/ScanningSystem.cpp
@@ -18,59 +18,58 @@ void ScanningSystem::update(float dt, ECS *ecs) {
 	std::set<Entity> ents_to_erase;
 
 	// Eureka!
-	// We need to scan for entities in rings of collision cells. That'll be way
+	// TODO: We need to scan for entities in rings of collision cells. That'll be way
 	// faster than scanning each of them.
 
+	/** Scan for nearby things with priority and add persuing component if we find something */
  	for (auto e = registered_entities.begin(); e != registered_entities.end(); e++) {
-			Entity ent = (Entity)*e;
+		Entity ent = (Entity)*e;
 
 		/*
 		std::cout << "Scanning System Processing for [" << ent << "]" << std::endl;
 		ecs->debug_cout_entity_state(ent);
 		*/
 
-		ScanningFor *scanning_for =
-				component_manager->get_component_data<ScanningFor>(ent);
+		ScanningFor *scanning_for = component_manager->get_component_data<ScanningFor>(ent);
 
 		float shortest_dist = INFINITY;
 		Entity sought_ent = -1;
 		Vec2 closest_pos;
 		Position *pos = component_manager->get_component_data<Position>(ent);
 
-	int earliest_scan_value_found = MAX_SCAN_VALUES+1; // Very simple priority system
+		int earliest_scan_value_found = MAX_SCAN_VALUES+1; // Very simple priority system
 
-	for (int i = 0; i < num_scannable; i++) {
-	Scannable s = comp_scannable.get_data_from_idx(i);
-	Entity scannable_ent = comp_scannable.get_entity_from_idx(i);
+		for (int i = 0; i < num_scannable; i++) {
+			Scannable s = comp_scannable.get_data_from_idx(i);
+			Entity scannable_ent = comp_scannable.get_entity_from_idx(i);
 
-			if (ent == scannable_ent)
-				continue;
+			if (ent == scannable_ent) { continue; }
 
-	bool sought = false;
-	float max_scan_range = 0;
+			bool sought = false;
+			float max_scan_range = 0;
 
-	for (int sv = 0; sv < MAX_SCAN_VALUES; sv++) {
-		if (s.scan_value == scanning_for->sought_scan_values[sv] && sv <= earliest_scan_value_found) {
-			sought = true;
-			earliest_scan_value_found = sv;
-			max_scan_range = scanning_for->max_range[sv];
-			break;
-		}
-	}
+			for (int sv = 0; sv < MAX_SCAN_VALUES; sv++) {
+				if (s.scan_value == scanning_for->sought_scan_values[sv] && sv <= earliest_scan_value_found) {
+					sought = true;
+					earliest_scan_value_found = sv;
+					max_scan_range = scanning_for->max_range[sv];
+					break;
+				}
+			}
 
-	if (!sought) continue;
+			if (!sought) continue;
 
-	Position p = *component_manager->get_component_data<Position>(scannable_ent);
-	float dist = pow((p.x - pos->x), 2) + pow((p.y - pos->y), 2);
+			Position p = *component_manager->get_component_data<Position>(scannable_ent);
+			float dist = pow((p.x - pos->x), 2) + pow((p.y - pos->y), 2);
 
-	// Squaring max scan range means i dont need to square root tee hee
-	if (dist < shortest_dist && (max_scan_range == -1 || dist < max_scan_range * max_scan_range)) {
-		sought_ent = scannable_ent;
-		shortest_dist = dist;
-		closest_pos.x = p.x;
-		closest_pos.y = p.y;
-	}
-	// std::cout << "[" << ent << "] Shortest Dist " << shortest_dist;
+			// Squaring max scan range means i dont need to square root tee hee
+			if (dist < shortest_dist && (max_scan_range == -1 || dist < max_scan_range * max_scan_range)) {
+				sought_ent = scannable_ent;
+				shortest_dist = dist;
+				closest_pos.x = p.x;
+				closest_pos.y = p.y;
+			}
+			// std::cout << "[" << ent << "] Shortest Dist " << shortest_dist;
 		}
 
 		if (shortest_dist != INFINITY) {
@@ -79,8 +78,7 @@ void ScanningSystem::update(float dt, ECS *ecs) {
 				ecs->remove_component_from_entity<g_Wandering>(ent);
 			}
 
-			// std::cout << "[" << ent << "] Seeking towards [" << sought_ent << "]"
-			// << std::endl;
+			// std::cout << "[" << ent << "] Seeking towards [" << sought_ent << "]" << std::endl;
 
 			Persuing *per = ecs->get_component_for_entity<Persuing>(ent);
 			per->desiredX = closest_pos.x;
@@ -90,8 +88,15 @@ void ScanningSystem::update(float dt, ECS *ecs) {
 		}
 	}
 
-	std::shared_ptr<ComponentArray<Persuing>> comp_persuing =
-			ecs->get_component_array<Persuing>();
+	// Clean up, remove ents
+	for (auto e = ents_to_erase.begin(); e != ents_to_erase.end(); e++) {
+		Entity ent = *e;
+		ecs->remove_component_from_entity<ScanningFor>(ent);
+	}
+
+
+	/** Process Pursuit */
+	std::shared_ptr<ComponentArray<Persuing>> comp_persuing = ecs->get_component_array<Persuing>();
 
 	for (int i = 0; i < comp_persuing->get_num_components(); i++) {
 		Entity persuing_ent = comp_persuing->get_entity_from_idx(i);
@@ -108,21 +113,20 @@ void ScanningSystem::update(float dt, ECS *ecs) {
 		Vec2 newPos = vecPos + dir;
 		// ecs->add_component_to_entity<Persuing>(ent, pe);
 
+		int pursuit_speed = 50;
+
+		// If i'm being carried, total effort changes the speed of pursuit
+
 		Transform *trans = component_manager->get_component_data<Transform>(persuing_ent);
-		trans->vel_x = -dir.x * 50;
-		trans->vel_y = -dir.y * 50;
+		trans->vel_x = -dir.x * pursuit_speed;
+		trans->vel_y = -dir.y * pursuit_speed;
 
 		if (!ecs->entity_has_component<GuyBrain>(persuing_ent)) {
-			trans->vel_x = -dir.x * 50;
-			trans->vel_y = -dir.y * 50;
+			trans->vel_x = -dir.x * pursuit_speed;
+			trans->vel_y = -dir.y * pursuit_speed;
 		}
 
 		// std::cout << "Trans [" << ent << "]" << std::endl;
 	}
 
-	// Clean up, remove ents
-	for (auto e = ents_to_erase.begin(); e != ents_to_erase.end(); e++) {
-		Entity ent = *e;
-		ecs->remove_component_from_entity<ScanningFor>(ent);
-	}
 }
