58fb8c653ccf6f2ec3471a6f7446d01d4d3bc949
Aidan <aidymouse4891@gmail.com>
2025-07-18 23:36:03 +1200

    [ScanningSystem] implement priority system properly + debug draw fn

--- include/systems/ScanningSystem.h
+++ include/systems/ScanningSystem.h
@@ -2,8 +2,10 @@
 
 #include <ecs/ECS.hpp>
 #include <ecs/System.hpp>
+#include <SDL3/SDL.h>
 
 class ScanningSystem : public System {
 public:
-  void update(float dt, ECS *ecs);
+	void update(float dt, ECS *ecs);
+	void debug_draw(SDL_Renderer *renderer, ECS *ecs);
 };
--- src/systems/ScanningSystem.cpp
+++ src/systems/ScanningSystem.cpp
@@ -6,6 +6,8 @@
 #include <set>
 #include <systems/ScanningSystem.h>
 #include <util/Vec2.hpp>
+#include <SDL3/SDL.h>
+#include <util/DrawFns.h>
 
 /** When scanning for an object, an entity will target the closest object with the highest priority 
  * (the order of scan values in the ScanningFor component is priority order) 
@@ -18,8 +20,7 @@ void ScanningSystem::update(float dt, ECS *ecs) {
 	std::set<Entity> ents_to_erase;
 
 	// Eureka!
-	// TODO: We need to scan for entities in rings of collision cells. That'll be way
-	// faster than scanning each of them.
+	// TODO: We need to scan for entities in rings of collision cells. That'll be way faster than scanning each of them.
 
 	/** Scan for nearby things with priority and add persuing component if we find something */
  	for (auto e = registered_entities.begin(); e != registered_entities.end(); e++) {
@@ -32,12 +33,12 @@ void ScanningSystem::update(float dt, ECS *ecs) {
 
 		ScanningFor *scanning_for = component_manager->get_component_data<ScanningFor>(ent);
 
-		float shortest_dist = INFINITY;
+		float dist_to_target = INFINITY;
 		Entity sought_ent = -1;
-		Vec2 closest_pos;
+		Vec2 target_pos;
 		Position *pos = component_manager->get_component_data<Position>(ent);
-
-		int earliest_scan_value_found = MAX_SCAN_VALUES+1; // Very simple priority system
+		SCAN_VALUES value_of_target_ent;
+		int priority_of_sought = MAX_SCAN_VALUES+1; // Only updated when one is found in range
 
 		for (int i = 0; i < num_scannable; i++) {
 			Scannable s = comp_scannable.get_data_from_idx(i);
@@ -45,46 +46,62 @@ void ScanningSystem::update(float dt, ECS *ecs) {
 
 			if (ent == scannable_ent) { continue; }
 
-			bool sought = false;
-			float max_scan_range = 0;
+			// Get priority of this entity
+			float cur_scanning_priority = MAX_SCAN_VALUES+1;
+			float max_scan_range = INFINITY;
 
 			for (int sv = 0; sv < MAX_SCAN_VALUES; sv++) {
-				if (s.scan_value == scanning_for->sought_scan_values[sv] && sv <= earliest_scan_value_found) {
-					sought = true;
-					earliest_scan_value_found = sv;
+				if (scanning_for->sought_scan_values[sv] == -1) { break; }
+				if (s.scan_value == scanning_for->sought_scan_values[sv]) {
+					cur_scanning_priority = sv;
 					max_scan_range = scanning_for->max_range[sv];
 					break;
 				}
 			}
 
-			if (!sought) continue;
+			if (max_scan_range == INFINITY) { continue; } // We are not looking for this kind of scannable
 
+			// Test if sought is in range
 			Position p = *component_manager->get_component_data<Position>(scannable_ent);
 			float dist = pow((p.x - pos->x), 2) + pow((p.y - pos->y), 2);
 
 			// Squaring max scan range means i dont need to square root tee hee
-			if (dist < shortest_dist && (max_scan_range == -1 || dist < max_scan_range * max_scan_range)) {
-				sought_ent = scannable_ent;
-				shortest_dist = dist;
-				closest_pos.x = p.x;
-				closest_pos.y = p.y;
+			if (max_scan_range != -1 && dist > max_scan_range * max_scan_range) { continue; } // Out of range!
+
+			if (cur_scanning_priority > priority_of_sought)	{
+				continue;
+			} else if (cur_scanning_priority < priority_of_sought) {
+				// Passthrough, we always want higher priority (earlier index = higher)
+			} else if (cur_scanning_priority == priority_of_sought) {
+				// Take shortest dist
+				if (dist > dist_to_target) { continue; }
 			}
-			// std::cout << "[" << ent << "] Shortest Dist " << shortest_dist;
+
+			sought_ent = scannable_ent;
+			dist_to_target = dist;
+			target_pos.x = p.x;
+			target_pos.y = p.y;
+			priority_of_sought = cur_scanning_priority;
+			//std::cout << "[" << ent << "] found " << s.scan_value << " with Shortest Dist " << shortest_dist << std::endl;
+			//ecs->debug_cout_entity_state(ent);
 		}
 
-		if (shortest_dist != INFINITY) {
+		if (sought_ent != -1) {
 			if (!ecs->entity_has_component<Persuing>(ent)) {
 				ecs->add_component_to_entity<Persuing>(ent, {0, 0});
 				ecs->remove_component_from_entity<g_Wandering>(ent);
 			}
 
-			// std::cout << "[" << ent << "] Seeking towards [" << sought_ent << "]" << std::endl;
+			//std::cout << "[" << ent << "] Seeking towards [" << sought_ent << "] (" << ecs->get_component_for_entity<Scannable>(sought_ent)->scan_value << ")" << std::endl;
 
 			Persuing *per = ecs->get_component_for_entity<Persuing>(ent);
-			per->desiredX = closest_pos.x;
-			per->desiredY = closest_pos.y;
+			per->desiredX = target_pos.x;
+			per->desiredY = target_pos.y;
 		} else {
 			ecs->remove_component_from_entity<Persuing>(ent);
+
+			//std::cout << "[" << ent << "] didnt find anything to scan (closest was " << dist_to_target << " away)" << std::endl;
+			//ecs->debug_cout_entity_state(ent);
 		}
 	}
 
@@ -127,6 +144,36 @@ void ScanningSystem::update(float dt, ECS *ecs) {
 		}
 
 		// std::cout << "Trans [" << ent << "]" << std::endl;
+	}
+
+}
+
+void ScanningSystem::debug_draw(SDL_Renderer *renderer, ECS *ecs) {
+
+	std::shared_ptr<ComponentArray<ScanningFor>> comp_scanningfor = ecs->get_component_array<ScanningFor>();
+	for (int i = 0; i < comp_scanningfor->get_num_components(); i++) {
+		Entity scanningfor_ent = comp_scanningfor->get_entity_from_idx(i);
+
+		//if (ecs->entity_has_component<Persuing>(scanningfor_ent)) { continue; }
+
+		ScanningFor sfor = comp_scanningfor->get_data_from_idx(i);
+		Position pos = *ecs->get_component_for_entity<Position>(scanningfor_ent);
+		for (int j=0; j<MAX_SCAN_VALUES; j++) {
+			if (sfor.max_range[j] == -1) { break; }
+  			DrawFns::RenderCircle(renderer, pos.x, pos.y, sfor.max_range[j]);
+		}
+
+	}
+
+	std::shared_ptr<ComponentArray<Persuing>> comp_persuing = ecs->get_component_array<Persuing>();
+
+	for (int i = 0; i < comp_persuing->get_num_components(); i++) {
+		Entity persuing_ent = comp_persuing->get_entity_from_idx(i);
+		Persuing per = comp_persuing->get_data_from_idx(i);
+
+		Position pos = *ecs->get_component_for_entity<Position>(persuing_ent);
+
+		SDL_RenderLine(renderer, pos.x, pos.y, per.desiredX, per.desiredY);
 	}
 
 }
