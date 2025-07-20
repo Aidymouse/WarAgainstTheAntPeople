ed694f1b643be6f8c649972c90038b38c02dee55
Aidan <aidymouse4891@gmail.com>
2025-07-20 13:26:34 +1200

    [GuyAnim] add offsets to splat

--- include/anim/GuyAnim.hpp
+++ include/anim/GuyAnim.hpp
@@ -12,13 +12,13 @@ struct {
 
   /** Squish */
   // squish.bmp
-  AnimFrame SQUISH0 = {{0, 0, 16, 16}, 0.035, &SQUISH1};
-  AnimFrame SQUISH1 = {{1 * 16, 0, 16, 16}, 0.035, &SQUISH2};
-  AnimFrame SQUISH2 = {{2 * 16, 0, 16, 16}, 0.035, &SQUISH3};
-  AnimFrame SQUISH3 = {{3 * 16, 0, 16, 16}, 0.035, &SQUISH4};
-  AnimFrame SQUISH4 = {{4 * 16, 0, 16, 16}, 0.035, &SQUISH5};
-  AnimFrame SQUISH5 = {{5 * 16, 0, 16, 16}, 0.035, &SQUISH6};
-  AnimFrame SQUISH6 = {{6 * 16, 0, 16, 16}, -1, nullptr};
+  AnimFrame SQUISH0 = {{0, 0, 16, 16}, 0.035, &SQUISH1, -7, -11};
+  AnimFrame SQUISH1 = {{1 * 16, 0, 16, 16}, 0.035, &SQUISH2, -7, -11};
+  AnimFrame SQUISH2 = {{2 * 16, 0, 16, 16}, 0.035, &SQUISH3, -7, -11};
+  AnimFrame SQUISH3 = {{3 * 16, 0, 16, 16}, 0.035, &SQUISH4, -7, -11};
+  AnimFrame SQUISH4 = {{4 * 16, 0, 16, 16}, 0.035, &SQUISH5, -7, -11};
+  AnimFrame SQUISH5 = {{5 * 16, 0, 16, 16}, 0.035, &SQUISH6, -7, -11};
+  AnimFrame SQUISH6 = {{6 * 16, 0, 16, 16}, -1, nullptr, -7, -11};
 
   /** Blow Up! */
 
